3bc31a3dd72c49a25756d19ce72bedf0ad99a7c1
honggquan24 <sinhnguyen2465@gmail.com>
2024-10-31 06:31:13 +0700

    6:31 31/10/2024

--- Report/Optimze/MDF.m
+++ Report/Optimze/MDF.m
@@ -1,7 +1,7 @@
+% Thiết lập các tham số DH (như đã cấu hình trong ví dụ trước)
 clear;
 clc;
 
-% Thay đổi các tham số từ bảng modified D-H
 th(1) = 0; d(1) = 0; a(1) = 0; alp(1) = 0;
 th(2) = 0; d(2) = 0; a(2) = 500; alp(2) = -pi/2;
 th(3) = 0; d(3) = 0; a(3) = 1700; alp(3) = 0;
@@ -17,13 +17,63 @@ L4 = Link([th(4), d(4), a(4), alp(4)], 'modified');
 L5 = Link([th(5), d(5), a(5), alp(5)], 'modified');
 L6 = Link([th(6), d(6), a(6), alp(6)], 'modified');
 
-% Tạo robot với các liên kết
 robot = SerialLink([L1, L2, L3, L4, L5, L6]);
 robot.name = '6-dof-robot';
 
-% Hiển thị và mô phỏng robot
-robot.plot([0,0,0,0,0,0]);
-robot.display();
-p = robot.fkine(th);
-q = robot.ikine(p);
-teach(robot);
+% Tạo dữ liệu huấn luyện
+num_samples = 1000;
+joint_angles = rand(num_samples, 6) * 2 * pi; % Ngẫu nhiên các góc khớp từ 0 đến 2π
+positions = zeros(num_samples, 3); % Lưu trữ vị trí (X, Y, Z)
+
+for i = 1:num_samples
+    robot.plot(joint_angles(i, :));
+    T = robot.fkine(joint_angles(i, :)); % Tính toán động học thuận
+    positions(i, :) = T.t'; % Lưu trữ tọa độ cuối (X, Y, Z)
+end
+
+% Lưu trữ dữ liệu thành file để dùng cho huấn luyện
+save('robot_training_data.mat', 'joint_angles', 'positions');
+
+
+% Load dữ liệu huấn luyện
+load('robot_training_data.mat');
+
+% Chia dữ liệu thành tập huấn luyện và kiểm tra
+train_ratio = 0.8;
+num_train = round(train_ratio * num_samples);
+X_train = joint_angles(1:num_train, :);
+Y_train = positions(1:num_train, :);
+X_test = joint_angles(num_train+1:end, :);
+Y_test = positions(num_train+1:end, :);
+
+% Xây dựng mô hình nơ-ron
+layers = [
+    featureInputLayer(6)
+    fullyConnectedLayer(64)
+    reluLayer
+    fullyConnectedLayer(64)
+    reluLayer
+    fullyConnectedLayer(3) % 3 đầu ra (X, Y, Z)
+    regressionLayer
+];
+
+options = trainingOptions('adam', ...
+    'MaxEpochs', 100, ...
+    'MiniBatchSize', 32, ...
+    'Plots', 'training-progress', ...
+    'Verbose', false);
+
+% Huấn luyện mô hình
+net = trainNetwork(X_train, Y_train, layers, options);
+
+% Đánh giá mô hình
+YPred = predict(net, X_test);
+mse = mean((YPred - Y_test).^2, 'all');
+fprintf('Mean Squared Error: %.4f\n', mse);
+
+
+% Dự đoán vị trí của robot cho một góc khớp mới
+new_angles = [pi/4, pi/6, pi/3, pi/2, pi/4, pi/3]; % Một ví dụ ngẫu nhiên
+predicted_position = predict(net, new_angles);
+disp('Vị trí dự đoán của đầu cuối robot:');
+disp(predicted_position);
---
+++ Report/Optimze/MDF2.m
@@ -0,0 +1,96 @@
+% Setup D-H parameters as per the article
+clear;
+clc;
+
+% Define the D-H parameters for each joint
+th(1) = 0; d(1) = 0; a(1) = 0; alp(1) = 0;
+th(2) = 0; d(2) = 0; a(2) = 500; alp(2) = -pi/2;
+th(3) = 0; d(3) = 0; a(3) = 1700; alp(3) = 0;
+th(4) = 0; d(4) = 2850; a(4) = 180; alp(4) = -pi/2;
+th(5) = 0; d(5) = 0; a(5) = 0; alp(5) = pi/2;
+th(6) = 0; d(6) = 0; a(6) = 0; alp(6) = -pi/2;
+
+% Define links using the specified D-H parameters in modified form
+L1 = Link([th(1), d(1), a(1), alp(1)], 'modified');
+L2 = Link([th(2), d(2), a(2), alp(2)], 'modified');
+L3 = Link([th(3), d(3), a(3), alp(3)], 'modified');
+L4 = Link([th(4), d(4), a(4), alp(4)], 'modified');
+L5 = Link([th(5), d(5), a(5), alp(5)], 'modified');
+L6 = Link([th(6), d(6), a(6), alp(6)], 'modified');
+
+% Create robot model
+robot = SerialLink([L1, L2, L3, L4, L5, L6]);
+robot.name = '6-dof-robot';
+
+% Parameters for quintuple polynomial interpolation
+t0 = 0; tf = 10; % Start and end times
+q0 = [0, 0, 0, 0, 0, 0]; % Initial joint angles
+qf = [pi/2, pi/4, pi/3, pi/6, pi/8, pi/10]; % Final joint angles
+
+% Quintuple polynomial coefficients calculation
+a0 = q0;
+a1 = zeros(1, 6);
+a2 = zeros(1, 6);
+a3 = (10 * (qf - q0)) / (tf^3);
+a4 = (-15 * (qf - q0)) / (tf^4);
+a5 = (6 * (qf - q0)) / (tf^5);
+
+% Generate trajectory over time
+dt = 0.1; % Time step
+time = t0:dt:tf;
+trajectory = zeros(length(time), 6); % To store joint angles over time
+
+for i = 1:length(time)
+    t = time(i);
+    trajectory(i, :) = a0 + a1 * t + a2 * t^2 + a3 * t^3 + a4 * t^4 + a5 * t^5;
+end
+
+% Plot the robot trajectory
+for i = 1:size(trajectory, 1)
+    robot.plot(trajectory(i, :));
+    pause(0.05);
+end
+
+% Save trajectory data for further analysis or training
+save('robot_trajectory_data.mat', 'time', 'trajectory');
+
+% Load data for neural network training
+load('robot_trajectory_data.mat');
+
+% Split data into training and testing sets
+train_ratio = 0.8;
+num_train = round(train_ratio * length(time));
+X_train = trajectory(1:num_train, :);
+Y_train = X_train; % Target is the same in this example
+X_test = trajectory(num_train+1:end, :);
+Y_test = X_test;
+
+% Define and train a neural network model to learn the trajectory
+layers = [
+    featureInputLayer(6)
+    fullyConnectedLayer(64)
+    reluLayer
+    fullyConnectedLayer(64)
+    reluLayer
+    fullyConnectedLayer(6) % Predicting joint angles
+    regressionLayer
+];
+
+options = trainingOptions('adam', ...
+    'MaxEpochs', 100, ...
+    'MiniBatchSize', 32, ...
+    'Plots', 'training-progress', ...
+    'Verbose', false);
+
+net = trainNetwork(X_train, Y_train, layers, options);
+
+% Evaluate model performance
+YPred = predict(net, X_test);
+mse = mean((YPred - Y_test).^2, 'all');
+fprintf('Mean Squared Error: %.4f\n', mse);
+
+% Predict robot configuration for a new input (example)
+new_angles = [pi/4, pi/6, pi/3, pi/2, pi/4, pi/3];
+predicted_angles = predict(net, new_angles);
+disp('Predicted joint angles:');
+disp(predicted_angles);
